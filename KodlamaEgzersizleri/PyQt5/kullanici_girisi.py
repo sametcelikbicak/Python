a9ebd95e034281d1199ccc220fb7b54142c90a2e
Samet ÇELİKBIÇAK <sametcelikbicak@outlook.com>
2018-02-03 13:54:14 +0300

    kullanıcı girişi tamamlandı.

--- KodlamaEgzersizleri/PyQt5/kullanici_girisi.py
+++ KodlamaEgzersizleri/PyQt5/kullanici_girisi.py
@@ -1,11 +1,20 @@
 import sys
+import sqlite3
 from PyQt5 import QtWidgets
+
 
 class Pencere(QtWidgets.QWidget):
 
     def __init__(self):
         super().__init__()
+        self.baglanti_olustur()
         self.init_ui()
+
+    def baglanti_olustur(self):
+        baglanti = sqlite3.connect("database.db")
+        self.cursor = baglanti.cursor()
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS UYELER(kullanici_adi TEXT, parola TEXT)")
+        baglanti.commit()
 
     def init_ui(self):
         self.kullanici_adi = QtWidgets.QLineEdit()
@@ -30,7 +39,21 @@ class Pencere(QtWidgets.QWidget):
 
         self.setLayout(h_box)
         self.setWindowTitle("Kullanıcı Girişi")
+        self.giris.clicked.connect(self.login)
         self.show()
+
+    def login(self):
+        adi = self.kullanici_adi.text()
+        parola = self.parola.text()
+        self.cursor.execute("SELECT * FROM UYELER WHERE kullanici_adi = ? and parola = ?",(adi,parola))
+        data = self.cursor.fetchall()
+        if len(data) == 0:
+            self.yazi_alani.setText("Böyle Bir Kullanıcı Yok\nLütfen Kontrol Ediniz.")
+        else:
+            self.yazi_alani.setText("Hoş Geldiniz " + adi)
+
+
+
 
 
 
